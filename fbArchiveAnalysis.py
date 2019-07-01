e918780de50dea42149b1000ffec532810b549c4
Bryan Walsh <bwals002@ucr.edu>
2019-06-30 21:57:48 -0700

    Update fbArchiveAnalysis.py
    6/30/2019:
    Added another data point

--- fbArchiveAnalysis.py
+++ fbArchiveAnalysis.py
@@ -1,6 +1,8 @@
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import norm
+import matplotlib.mlab as mlab
 import pandas.plotting._converter as pandacnv
 pandacnv.register()
 
@@ -12,17 +14,36 @@ with open('message_1.json') as file:
     for i in msgs:
         timestamp = pd.to_datetime(i["timestamp_ms"], unit='ms')
         doy = timestamp.dayofyear - 320
-
         doy = ((timestamp.year - 2017) * 365) + doy
 
         timestamps.append(doy)
 
         # if 'content' in msgs:
         #     print(i["content"])
-        # wordcloud
 
+# calculate msg streak
+streakEnd = []
+for i in range(len(timestamps) - 1):
+    if (timestamps[i+1] - timestamps[i]) > 1:
+        streakEnd.append(timestamps[i]) # last day of streak
 
-plt.hist(timestamps, bins=80)
+if not streakEnd:
+    print("No missed days")
+else:
+    print(*streakEnd, sep=', ')
+
+(mu, sigma) = norm.fit(timestamps)
+
+plt.hist(timestamps,
+         bins=80,
+         align='mid')
+y = mlab.normpdf(80, mu, sigma)
+l = plt.plot(80, y, 'r--', linewidth=2)
+plt.grid(True)
+plt.axis([0, 730, 0, 2000])
+plt.ylabel('Msg count')
+plt.xlabel('Day since first message')
+plt.text(75, 1600, 'bin=3.5days')
 plt.show()
 
 # Graphs messages by frequency per day of year - some values hardcoded.
