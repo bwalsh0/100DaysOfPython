af086a4138a7e2a5593cbfcc690613f376085d40
Bryan Walsh <bwals002@ucr.edu>
2019-07-03 18:34:54 -0700

    Update dot_com_dictionary.py
    Return if domain not reg. (lim 100)

--- dot_com_dictionary.py
+++ dot_com_dictionary.py
@@ -1,17 +1,46 @@
 import socket
-from os import system
+import sys
+import whois
+import socks
 
-socket.setdefaulttimeout(5)
+socket.setdefaulttimeout(2)
+
+sockproto = socket.AF_INET6
+s = socks.socksocket(sockproto)
+s.settimeout(2)
 
 i = 0
 en_dictionary = open('words_alpha.txt')
 for line in en_dictionary:
     i += 1
-    if i == 10:
+    if i == 100:
         break
     word = line.splitlines()
-    print(word)
+
+    suf = '.com'
+    url = word[0] + suf
+
+    try:
+        resp = whois.whois(url)
+    except whois.parser.PywhoisError:
+        print(word)
+    except socket.timeout:
+        print(word, " TIMEOUT")
+        continue
+
+    # if not resp.name:
+    #     print(word)
+    # else:
+    #     pass
+        # print(resp.name)
+
+    # resp = subprocess.call(['ping', '-l', '2', '-n', '2', '-w', '3', url])
+    #
+    # if resp == 1:
+    #     print(word)
 
 en_dictionary.close()
+sys.exit(0)
+
 
 # WIP: Ping every *.com domain name in the english dictionary to check which ones are still unclaimed
