f1740ce5bf768c275db17ef1a1f16cd12e4a066f
Bryan Walsh <bwals002@ucr.edu>
2019-07-04 00:16:46 -0700

    Output to .txt, catch exceptions

--- dot_com_dictionary.py
+++ dot_com_dictionary.py
@@ -11,21 +11,26 @@ s.settimeout(2)
 
 i = 0
 en_dictionary = open('words_alpha.txt')
+file_output = open('dot_coms.txt', 'a')
+
 for line in en_dictionary:
     i += 1
-    if i == 100:
-        break
+    # if i == 100:
+    #     break
     word = line.splitlines()
 
-    suf = '.com'
-    url = word[0] + suf
+    url = word[0] + '.com'
 
     try:
-        resp = whois.whois(url)
+        whois.whois(url)
     except whois.parser.PywhoisError:
-        print(word)
+        file_output.write(word[0] + '\n')
+        print(i)
+        # print(word)
     except socket.timeout:
-        print(word, " TIMEOUT")
+        file_output.write(word[0] + ' TIMEOUT' + '\n')
+        print(i)
+        # print(word, " TIMEOUT")
         continue
 
     # if not resp.name:
@@ -40,7 +45,7 @@ for line in en_dictionary:
     #     print(word)
 
 en_dictionary.close()
+file_output.close()
 sys.exit(0)
-
 
 # WIP: Ping every *.com domain name in the english dictionary to check which ones are still unclaimed
