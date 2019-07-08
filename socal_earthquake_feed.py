850e6e51b9c1165871495ced631e6f6b34b8c385
Bryan Walsh <bwals002@ucr.edu>
2019-07-07 19:29:21 -0700

    Added lat/long bounds, other improvements

--- socal_earthquake_feed.py
+++ socal_earthquake_feed.py
@@ -1,44 +1,57 @@
 import json, urllib.request
 import time
 from time import sleep
+ 
+# Sample output:
+# New earthquakes in last interval: 07/07/19 19:24
+# >> 2.54  --  17km ESE of Little Lake, CA  --  3 minutes ago
 
-def parseEvents(features, timeNow, timeLast):
-    for event in features:
+def parseEvents(features, timeNow, lastId) -> int:
+    latBounds = [32.30, 35.95]
+    longBounds = [-123.03, -114.00]
+    # Bounds define a rough 2d square on the globe enclosing all of CA from 
+    # San Luis Obispo, CA to Baja Mexico, MX; and the Pacific Ocean to Las Vegas, NV.
+
+    for i, event in enumerate(features):
         timeSince = round((timeNow - event["properties"]["time"]) / 60000)
-        if timeLast == event["properties"]["time"]:
-            print("No new updates")
-            # no new updates, don't parse json
-            return timeLast
-        magnitude = event["properties"]["mag"]
-        location = event["properties"]["place"]
-        coords = event["geometry"]["coordinates"]
+        currId = event["properties"]["code"]
 
-        print(magnitude, " -- ", location, " -- ", timeSince, "minutes ago")
-        # new update in past hour, update counter
-        return event["properties"]["time"]
-        # for i in range(2):
-        #     if event[i]
+        if lastId == currId:
+            # No new updates, don't output list
+            if i == 0:
+                print(">> No new earthquakes")
+            return features[0]["properties"]["code"]
+
+        if i == 0:
+            timeFmt = time.strftime('%D %H:%M')
+            print("New earthquakes in last interval:", timeFmt)
+
+        # Filter only locally-relevant results
+        coords = event["geometry"]["coordinates"]
+        if ((coords[0] > longBounds[0]) and (coords[0] < longBounds[1])) and \
+            ((coords[1] > latBounds[0]) and (coords[1] < latBounds[1])):
+            magnitude = event["properties"]["mag"]
+            location = event["properties"]["place"]
+
+            # New update in past hour, print and continue until lastId == currId
+            print(">>", magnitude, " -- ", location, " -- ", timeSince, "minutes ago")
+    
+    return features[0]["properties"]["code"]
 
 def main():
     pollFreq = 55 # time in sec. between json requests
     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson'
-    runtimeCount = 0
-    timeLast = 0
-
-    latBounds = [32.30, 35.95]
-    longBounds = [-123.03, -114.00]
+    lastId = 0
 
     while True:
         with urllib.request.urlopen(url) as geojson:
             jsonResponse = json.load(geojson)
             metadata = jsonResponse["metadata"]
-            currentCount = metadata["count"]
             features = jsonResponse["features"]
 
-            print("Retrieved GeoJSON")
+            print("-- Retrieved GeoJSON --")
 
-            print("New earthquakes in last hour")
-            timeLast = parseEvents(features, metadata["generated"], timeLast)
+            lastId = parseEvents(features, metadata["generated"], lastId)
 
         sleep(pollFreq)
 
