8e640547e0d30def4207c1595cc3aff223878e99
Bryan Walsh <bwals002@ucr.edu>
2019-07-08 21:39:35 -0700

    Always-on state, pull-down res activates
    7/8/2019

--- rpi_dht.py
+++ rpi_dht.py
@@ -1,17 +1,50 @@
-import sys
+import time
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+import Adafruit_CharLCD as LCD
 
-pin = 23 # GPIO 23
+GPIO.setwarnings(False)
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
+lcd_rs = 26
+lcd_en = 19
+lcd_d4 = 13
+lcd_d5 = 6
+lcd_d6 = 5
+lcd_d7 = 11
+lcd_backlight = 2
 
-temperature = temperature * 9/5.0 + 32
+lcd_columns = 16
+lcd_rows = 2
 
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Sensor/pin detected but reading failed')
-    sys.exit(1)
+sensor = 22     # sensor model
+sPin = 14        # sensor signal gpio pin
+bPin = 15     # toggle button gpi pin
+
+GPIO.setup(bPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.clear()
+
+while True:
+    bState = GPIO.input(bPin)
+    if bState:
+        lcd.message('Reading...')
+        time.sleep(0.5)
+        lcd.clear()
+        humidity, temp = Adafruit_DHT.read_retry(sensor, sPin)
+        temp = temp * 9/5.0 + 32
+        time.sleep(0.5)
+
+        if humidity is not None and temp is not None:
+            print('Temp:{0:0.1f}  Hum:{1:0.1f}%'.format(temp, humidity))
+            lcd.message('T:{0:0.1f}F\nH:{1:0.1f}%'.format(temp, humidity))
+            time.sleep(5)
+        else:
+            lcd.message('Failed to read sensor')
+        lcd.clear()
+    time.sleep(1)
 
 # wip room humidity/temp monitor & controller
 # uses Adafruit DHT library: https://github.com/adafruit/Adafruit_Python_DHT
