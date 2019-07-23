e209f4bf670a65112f5772a91748c209a620b5fd
Bryan Walsh <bwals002@ucr.edu>
2019-07-22 20:24:15 -0700

    Update statement_visualizer.py
    7/22/2019

--- statement_visualizer.py
+++ statement_visualizer.py
@@ -3,36 +3,49 @@ import matplotlib.pyplot as plt
 
 i = 0
 date, expenses = [], []
+labels, labelExpense = [], []
 
-with open(r'statement.CSV') as csvfile:
-    reader = csv.DictReader(csvfile)
 
-    for row in reader:
-        if row.get('Type') == 'CHASE_TO_PARTNERFI' or \
-                row.get('Type') == 'QUICKPAY_DEBIT' or \
-                row.get('Type') == 'ACH_DEBIT':
-            continue
-        amt = float(row.get('Amount'))
-        if amt is not None and amt < 0:
-            amt *= -1.00
-            day = row.get('Posting Date')
-            if len(date) > 0 and day == date[-1]:
-                expenses[-1] += amt
-            else:
-                expenses.append(amt)
-                date.append(day)
-        # i += 1
-        # if i == 100:
-        #     csvfile.close
-        #     break
-    print(date, expenses)
+def main():
+    with open(r'statement.CSV') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-plt.plot(date, expenses, scalex=True, scaley=True)
+        for row in reader:
+            if row.get('Type') == 'CHASE_TO_PARTNERFI' or \
+                    row.get('Type') == 'QUICKPAY_DEBIT' or \
+                    row.get('Type') == 'ACH_DEBIT':
+                continue
 
-# hist = plt.plot(80, y, 'r--', linewidth=2)
-plt.grid(True)
-# plt.axis([0, 730, 0, 2000])
-plt.ylabel('Expense')
-plt.xlabel('Date')
-# plt.text(75, 1600, 'bin=3.5days')
-plt.show()
+            amt = float(row.get('Amount'))
+
+            if amt is not None and amt < 0:
+                amt *= -1.00
+                label = row.get('Label')
+                day = row.get('Posting Date')
+
+                # Expenses over time (per day)
+                # if len(date) > 0 and day == date[-1]:
+                #     expenses[-1] += amt
+                # else:
+                #     expenses.append(amt)
+                #     date.append(day)
+
+                if len(labels) > 0 and label == labels[-1]:
+                    labelExpense[-1] += amt
+                else:
+                    labelExpense.append(amt)
+                    labels.append(label)
+
+    with open('statement_OUTPUT.CSV', 'w+') as csvout:
+        writer = csv.writer(csvout)
+        writer.writerows(zip(labels, labelExpense))
+
+    plt.plot(labels, labelExpense)
+    plt.ylabel('Expense')
+    plt.xlabel('Date')
+    plt.show()
+
+    exit()
+
+
+main()
