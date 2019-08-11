ffb513b096be353c06018bc2847097e774bcfdb4
Bryan Walsh <bwals002@ucr.edu>
2019-08-10 23:26:13 -0700

    Update rpi_dht.py
    8/10/2019

--- rpi_dht.py
+++ rpi_dht.py
@@ -2,6 +2,7 @@ import time
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import Adafruit_CharLCD as LCD
+from datetime import datetime
 
 GPIO.setwarnings(False)
 
@@ -27,24 +28,39 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 
 lcd.clear()
 
-while True:
-    bState = GPIO.input(bPin)
-    if bState:
-        lcd.message('Reading...')
-        time.sleep(0.5)
-        lcd.clear()
-        humidity, temp = Adafruit_DHT.read_retry(sensor, sPin)
-        temp = temp * 9/5.0 + 32
-        time.sleep(0.5)
 
-        if humidity is not None and temp is not None:
-            print('Temp:{0:0.1f}  Hum:{1:0.1f}%'.format(temp, humidity))
-            lcd.message('T:{0:0.1f}F\nH:{1:0.1f}%'.format(temp, humidity))
-            time.sleep(5)
-        else:
-            lcd.message('Failed to read sensor')
-        lcd.clear()
-    time.sleep(1)
+def main():
+    while True:
+        poll_sensor()
+        time.sleep(1800)
+    
+    # average_rop()
 
-# wip room humidity/temp monitor & controller
-# uses Adafruit DHT library: https://github.com/adafruit/Adafruit_Python_DHT
+def poll_sensor():
+        # bState = GPIO.input(bPin)
+        # if bState:
+    lcd.message('Reading...')
+    time.sleep(0.5)
+    lcd.clear()
+    humidity, temp = Adafruit_DHT.read_retry(sensor, sPin)
+    temp = temp * 9/5.0 + 32
+    time.sleep(0.5)
+
+    if humidity is not None and temp is not None:
+        print('Temp:{0:0.1f}  Hum:{1:0.1f}%'.format(temp, humidity))
+        lcd.message('T:{0:0.1f}F\nH:{1:0.1f}%'.format(temp, humidity))
+        time.sleep(5)
+    else:
+        lcd.message('Failed to read sensor')
+        lcd.clear()
+        time.sleep(1)
+    
+    time = datetime.now()
+    store_rop(temp, humidity, time)
+
+def store_rop(temp: float, hum: float, time: str):
+    with open(r'sensor_log.txt', 'a') as log:
+        rop = str(temp) + ',' + str(hum) + ',' + str(time) + '\n'
+        log.write(rop)
+
+main()
