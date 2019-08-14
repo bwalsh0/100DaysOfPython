b565179854e7b97317fce02e66740618a0d7799e
Bryan Walsh <bwals002@ucr.edu>
2019-08-13 22:11:25 -0700

    Update rpi_dht.py
    8/13/2019 (2)

--- rpi_dht.py
+++ rpi_dht.py
@@ -5,26 +5,19 @@ from RPLCD import CharLCD
 from datetime import datetime
 import pytz
 
+POLL_RATE = 30 * 60     # ROP in minutes
+LCD_DATA_PINS = [13, 6, 5, 11]
+S_PIN, SENSOR, B_PIN = 14, 22, 23   # Sensor signal, sensor data, button pin
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(B_PIN, GPIO.IN,
+           pull_up_down=GPIO.PUD_DOWN)
 
-POLL_RATE = 30 * 60     # ROP in minutes
-
-lcd_rs = 26
-lcd_en = 19
-lcd_data = [13, 6, 5, 11]
-
-sensor = 22     # sensor model
-sPin = 14        # sensor signal gpio pin
-bPin = 23     # toggle button gpi pin
-
-GPIO.setup(bPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-lcd = CharLCD(pin_rs=lcd_rs, pin_e=lcd_en, pins_data=lcd_data,
-numbering_mode=GPIO.BCM,
-auto_linebreaks=True,
-cols=16, rows=2, dotsize=8)
-
+lcd = CharLCD(pin_rs=26, pin_e=19, pins_data=LCD_DATA_PINS,
+              numbering_mode=GPIO.BCM, dotsize=8,
+              auto_linebreaks=True,
+              cols=16, rows=2)
 lcd.clear()
 
 
@@ -32,63 +25,53 @@ def main():
     while True:
         poll_sensor()
         idle_sensor()   # Hold for 30 min, listen for button down
-        # print("Looped")
     # average_rop()
 
 
 def poll_sensor():
+    temp, humidity = read_sensor()
     timeNow = datetime.now(pytz.timezone('US/Pacific')).strftime("%m/%d/%Y %H:%M")
-    # lcd.write_string('Polling ROP...')
-    humidity, temp = Adafruit_DHT.read_retry(sensor, sPin)
-    temp = temp * 9/5.0 + 32
-    time.sleep(0.5)
-
-    if humidity is not None and temp is not None:
-        print('Temp:{0:0.1f} Hum:{1:0.1f}%'.format(temp, humidity), timeNow)
-        time.sleep(5)
-    else:
-        temp, humidity = -1, -1
-        lcd.write_string('Failed to read sensor')
-        print("Failed to read sensor at", timeNow)
-        time.sleep(5)
-    lcd.clear()
-
-    store_rop(temp, humidity, timeNow)
-
-
-def store_rop(temp: float, hum: float, time: str):
+    print('Temp:{0}\u00b0F Hum:{1}%'.format(temp, humidity), timeNow)
     with open(r'sensor_log.txt', 'a') as log:
-        output = str(temp) + ',' + str(hum) + ',' + time + '\n'
+        output = str(temp) + ',' + str(humidity) + ',' + timeNow + '\n'
         log.write(output)
+        log.close()
 
 
 def idle_sensor():
     counter = 0
-
     while counter <= POLL_RATE:
-        if GPIO.input(bPin) == GPIO.HIGH:
-            print("Pressed")
-            read_sensor(counter)
-
+        if GPIO.input(B_PIN) == GPIO.HIGH:
+            trigger_sensor(counter)
         counter += 1
         time.sleep(1)
 
 
-def read_sensor(counter: int):
-    lcd.clear()
-    lcd.write_string('Reading sensor..')
-    humidity, temp = Adafruit_DHT.read_retry(sensor, sPin)
+def read_sensor() -> (float, float):
+    humidity, temp = Adafruit_DHT.read_retry(SENSOR, S_PIN)
     temp = temp * 9/5.0 + 32
     time.sleep(0.5)
 
-    lcd.clear()
-    if humidity is not None and temp is not None:
-        # print('Temp:{0:0.1f}  Hum:{1:0.1f}%'.format(temp, humidity))
-        lcd.write_string('T:{0:0.1f}F H:{1:0.1f}%, c{2}'.format(temp, humidity, counter))
-        time.sleep(5)
-    else:
+    if humidity is None and temp is None:
+        temp, humidity = -1, -1
         lcd.write_string('Failed to read sensor')
-        time.sleep(1)
+        time.sleep(2)
+        lcd.clear()
+        return temp, humidity
+    temp = round(temp, 2)
+    humidity = round(humidity, 2)
+
+    return temp, humidity
+
+
+def trigger_sensor(counter: int):
+    lcd.clear()
+    print(">> Reading sensor..")
+    lcd.write_string('Reading sensor..')
+    temp, humidity = read_sensor()
+    lcd.clear()
+    lcd.write_string('{0}F, {1}% Hum, c{2}'.format(temp, humidity, counter))
+    time.sleep(5)
     lcd.clear()
 
 
